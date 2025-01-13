52d4fb2b3497d10aea8c5eb82d041ccf6215cae8
Marcin Koziński <marcin.m.kozinski+gh@gmail.com>
2025-01-13 12:52:45 +0100

    Revert "build(gradle): actually use r8 dependency"

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,11 @@
 pluginManagement {
-    buildscript {
-        repositories {
-            maven {
-                name = "R8 releases"
-                url = uri("https://storage.googleapis.com/r8-releases/raw")
-            }
-        }
-        dependencies {
-            classpath("com.android.tools:r8:8.8.18")
-        }
-    }
     repositories {
         gradlePluginPortal()
         google()
+        maven {
+            name = "R8 releases"
+            url = uri("https://storage.googleapis.com/r8-releases/raw")
+        }
     }
 }
 
